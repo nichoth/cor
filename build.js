4b561565aad1dbb77a1ed3824436eb0ddf60bb06
nichoth <nichoth@gmail.com>
2021-04-17 21:21:41 -0700

    it works

--- build.js
+++ build.js
@@ -4,39 +4,48 @@ var path = require('path')
 var matter = require('gray-matter')
 var mkdirp = require('mkdirp')
 
-// do the pages (from md files)
+buildThem(
+    __dirname + '/src/_pages',
+    __dirname + '/public',
+    __dirname + '/src/_index.html',
+    makeHs
+)
 
-// read the index template
-// then pipe it through hs with the content
-
-fs.readdir(__dirname + '/src/_pages', function (err, files) {
-    if (err) throw err
-    console.log('files:  ', files)
-
-    files.forEach(fileName => {
-        var _path = __dirname + '/src/_pages/' + fileName
-        var baseName = path.basename(fileName, '.md')
-
-        fs.readFile(_path, 'utf8', (err, file) => {
-            if (err) throw err
-
-            mkdirp.sync(__dirname + '/public/' + baseName)
-
-            var hs = hyperstream({
-                body: {
-                    class: { append: baseName }
-                },
-                '#content': {
-                    _appendHtml: matter(file).content
-                }
-            })
-            var ws = fs.createWriteStream(__dirname + '/public/' + baseName + 
-                '/index.html')
-            var rs = fs.createReadStream(__dirname + '/src/_index.html')
-
-            rs.pipe(hs).pipe(ws)
-        })
+// a fn that returns a hs instance
+function makeHs (file, baseName) {
+    return hyperstream({
+        body: {
+            class: { append: baseName }
+        },
+        '#content': {
+            _appendHtml: matter(file).content
+        }
     })
+}
 
-})
+function buildThem (inputDir, outputDir, templateFile, makeHs) {
+    fs.readdir(inputDir, function (err, files) {
+        if (err) throw err
+        console.log('files:  ', files)
 
+        files.forEach(fileName => {
+            var _path = path.join(inputDir, fileName)
+            var baseName = path.basename(fileName, '.md')
+
+            fs.readFile(_path, 'utf8', (err, file) => {
+                if (err) throw err
+
+                var outFileDir = outputDir + '/' + baseName
+                mkdirp.sync(outFileDir)
+
+                var hs = makeHs(file, baseName)
+
+                var ws = fs.createWriteStream(outFileDir + '/index.html')
+                var rs = fs.createReadStream(templateFile)
+
+                rs.pipe(hs).pipe(ws)
+            })
+        })
+
+    })
+}
