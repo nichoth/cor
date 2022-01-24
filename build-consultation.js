71e510cf3e25aa66d628f4ed3b2b0d9c4e9bc684
nichoth <nichoth@gmail.com>
2022-01-23 17:59:56 -0800

    style

--- build-consultation.js
+++ build-consultation.js
@@ -40,7 +40,7 @@ function buildConsultation () {
 
                     <!-- Calendly inline widget begin -->
                     <div class="calendly-inline-widget" data-url="https://calendly.com/stillbrilliance?text_color=131311&primary_color=79c8a9" style="min-width:320px;height:630px;"></div>
-                    <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
+                        <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
                     <!-- Calendly inline widget end -->
 
                 </div>
